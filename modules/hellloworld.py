d2bfdf915770e6aaedc60b6cdfd111b16ca93e0d
Hadi <hadi@hypixel.lol>
2025-11-26 21:25:37 -0500

    Add hello command to greet users

--- modules/hellloworld.py
+++ modules/hellloworld.py
@@ -1,1 +1,8 @@
+# /hello example command
+from discord import Interactixxon
+from discord.ext import commands
 
+def register(client: commands.Bot):
+    @client.tree.command(name="hello", description="Sends a greeting message!")
+    async def hello(interaction: Interaction):
+        await interaction.response.send_message("Hello there!")
